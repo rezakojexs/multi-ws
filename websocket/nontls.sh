fd229d9fb90ecf6b807102cc06da28f01dd8a5c7
rezakojexs <80284781+rezakojexs@users.noreply.github.com>
2023-02-21 18:47:35 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -21,7 +21,7 @@ echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/godtrex99/multi-ws/main/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/rezakojexs/multi-ws/main/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
